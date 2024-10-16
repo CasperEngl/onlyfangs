a589ae5aa3ac1def2fc2e111dfe41296ea14c155
Casper Engelmann <c.engelmann@custimy.io>
2024-10-16 03:26:24 +0200

    Refactor MySQL connection pool configuration

--- app/db/client.ts
+++ app/db/client.ts
@@ -1,8 +1,17 @@
 import mysql from "mysql2/promise";
 
 // Create a MySQL connection pool
-export const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  database: "onlyfangs",
-  connectionLimit: 10,
-});
+export const pool = mysql.createPool(
+  process.env.NODE_ENV === "production"
+    ? {
+        user: "root",
+        password: "rootpassword",
+        port: 3306,
+        database: "onlyfangs",
+      }
+    : {
+        uri: process.env.DATABASE_URL,
+        database: "onlyfangs",
+        connectionLimit: 10,
+      }
+);
