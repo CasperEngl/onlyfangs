4e750718cf15d342b8ed886374aec848cd1385ea
Casper Engelmann <c.engelmann@custimy.io>
2024-10-17 21:49:15 +0200

    Add invite code authentication using CredentialsProvider in NextAuth configuration and refactor related code

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import invariant from "invariant";
 import NextAuth from "next-auth";
-import { authConfig } from "~/auth-config";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { z } from "zod";
+import { getPlayerByInviteCode } from "~/db";
+import { pool } from "~/db/client";
 import {
   Accounts,
   Authenticators,
@@ -15,12 +18,39 @@ invariant(process.env.AUTH_DISCORD_ID, "Missing AUTH_DISCORD_ID");
 invariant(process.env.AUTH_DISCORD_SECRET, "Missing AUTH_DISCORD_SECRET");
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: process.env.NODE_ENV === "development",
   adapter: DrizzleAdapter(db, {
-    usersTable: Users as any,
-    accountsTable: Accounts as any,
-    authenticatorsTable: Authenticators as any,
-    sessionsTable: Sessions as any,
+    usersTable: Users as never,
+    accountsTable: Accounts as never,
+    authenticatorsTable: Authenticators as never,
+    sessionsTable: Sessions as never,
     verificationTokensTable: VerificationTokens,
   }),
-  ...authConfig,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Invite Code",
+      credentials: {
+        inviteCode: { label: "Invite Code", type: "text" },
+      },
+      async authorize(credentials) {
+        const inviteCode = z.string().parse(credentials.inviteCode);
+
+        const user = await getPlayerByInviteCode(pool, {
+          code: inviteCode,
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        return {
+          ...user,
+          id: user?.id.toString(),
+        };
+      },
+    }),
+  ],
 });
