38b535f367e474d31af36703cf9e37768fc8c885
Casper Engelmann <c.engelmann@custimy.io>
2024-10-17 22:35:02 +0200

    Delete low_plazm.sql and add volatile_redwing.sql

---
+++ drizzle/0000_volatile_redwing.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS "classes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"slug" text NOT NULL,
+	CONSTRAINT "classes_name_unique" UNIQUE("name"),
+	CONSTRAINT "classes_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "invite_codes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"code" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"used_at" timestamp,
+	CONSTRAINT "invite_codes_code_unique" UNIQUE("code")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "races" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"slug" text NOT NULL,
+	CONSTRAINT "races_name_unique" UNIQUE("name"),
+	CONSTRAINT "races_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"race_id" integer,
+	"class_id" integer,
+	"invite_code_id" integer,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_race_id_races_id_fk" FOREIGN KEY ("race_id") REFERENCES "public"."races"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_class_id_classes_id_fk" FOREIGN KEY ("class_id") REFERENCES "public"."classes"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_invite_code_id_invite_codes_id_fk" FOREIGN KEY ("invite_code_id") REFERENCES "public"."invite_codes"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
